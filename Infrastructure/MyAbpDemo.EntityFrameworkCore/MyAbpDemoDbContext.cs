25907f6edc46b511f0c379b6be748f73965106b0
yungchu <jianlive@qq.com>
2018-11-28 22:31:46 +0800

    迁移文件初始化

--- Infrastructure/MyAbpDemo.EntityFrameworkCore/MyAbpDemoDbContext.cs
+++ Infrastructure/MyAbpDemo.EntityFrameworkCore/MyAbpDemoDbContext.cs
@@ -43,8 +43,14 @@ namespace MyAbpDemo.EFCore
                 TeacherId = 1
             });
 
-
-
+            modelBuilder.Entity<Student>().HasData(new Student()
+            {
+                Id=2,
+                Name = "学生2",
+                Age = 36,
+                IsActive = true,
+                TeacherId = 1
+            });
         }
     }
 }
